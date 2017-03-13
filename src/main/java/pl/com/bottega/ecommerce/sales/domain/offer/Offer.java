422a6a6b9daaca82c8bfeadfc4beb1b8b60c9974
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-03-13 23:30:45 +0100

    Zmiana nazwy zmiennej availableItems + poprawiona metoda hashCode w klasie Java (wziecie pod uwage unavailableItems)

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Offer {
-	private List<OfferItem> availabeItems = new ArrayList<OfferItem>();
+	private List<OfferItem> availableItems = new ArrayList<OfferItem>();
 
 	private List<OfferItem> unavailableItems = new ArrayList<OfferItem>();
 
 	public Offer(List<OfferItem> availabeItems, List<OfferItem> unavailableItems) {
-		this.availabeItems = availabeItems;
+		this.availableItems = availabeItems;
 		this.unavailableItems = unavailableItems;
 	}
 
 	public List<OfferItem> getAvailabeItems() {
-		return availabeItems;
+		return availableItems;
 	}
 
 	public List<OfferItem> getUnavailableItems() {
@@ -25,7 +25,8 @@ public class Offer {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((availabeItems == null) ? 0 : availabeItems.hashCode());
+		result = prime * result + ((availableItems == null) ? 0 : availableItems.hashCode());
+		result = prime * result + ((unavailableItems == null) ? 0 : unavailableItems.hashCode());
 		return result;
 	}
 
@@ -38,10 +39,10 @@ public class Offer {
 		if (getClass() != obj.getClass())
 			return false;
 		Offer other = (Offer) obj;
-		if (availabeItems == null) {
-			if (other.availabeItems != null)
+		if (availableItems == null) {
+			if (other.availableItems != null)
 				return false;
-		} else if (!availabeItems.equals(other.availabeItems))
+		} else if (!availableItems.equals(other.availableItems))
 			return false;
 		return true;
 	}
@@ -54,10 +55,10 @@ public class Offer {
 	 * @return
 	 */
 	public boolean sameAs(Offer seenOffer, double delta) {
-		if (!(availabeItems.size() == seenOffer.availabeItems.size()))
+		if (!(availableItems.size() == seenOffer.availableItems.size()))
 			return false;
 
-		for (OfferItem item : availabeItems) {
+		for (OfferItem item : availableItems) {
 			OfferItem sameItem = seenOffer.findItem(item.getProduct().getProductId());
 			if (sameItem == null)
 				return false;
@@ -69,7 +70,7 @@ public class Offer {
 	}
 
 	private OfferItem findItem(String productId) {
-		for (OfferItem item : availabeItems) {
+		for (OfferItem item : availableItems) {
 			if (item.getProduct().getProductId().equals(productId))
 				return item;
 		}
